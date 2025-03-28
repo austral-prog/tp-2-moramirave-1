4d430da589a12c4f69a0248f94fe244fd164438d
moramirave <mmirave@mail.austral.edu.ar>
2025-03-28 11:56:28 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    nombre=first_name + " " + last_name
+
+    print(nombre.lower())
+    print(nombre.title())
+    print(nombre.upper())
+    print(2*"\t" + nombre.lower())
